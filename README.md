1c0c82fb0bd91a65a83064c83911afee0a3f29b6
zack <shiqizhenyes@163.com>
2017-12-16 17:49:49 +0800

    递归
    斐波拉契数列

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-PythonStudy
+#  PythonStudy
+### 学习进度
+1 函数的返回值 <br>
+2 高级函数 <br>
+3 作用域 LEGB <br>
+4 函数参数 常用类型，函数名 <br>
+5 递归函数
--- day004/Function.py
+++ day004/Function.py
@@ -1,5 +1,6 @@
 import LogUtil
 
+# 高级函数
 count = 1024
 
 
@@ -13,6 +14,61 @@ def add(*args):
         print(arg)
 
 
+def add(a, b):
+    return a + b
+
+
+def add_height(a, b, func):
+    return a + b + func(a, b)
+
+
+def multiplication(a, b):
+    return a * b
+
+
+def multiplication_height(a, b):
+    return add(multiplication(a, b), multiplication(b, a))
+
+
+def factorial(a):
+    b = 1
+    for i in range(1, a + 1):
+        b = b * i
+    return b
+
+
+# 递归函数
+def factorial_high(a):
+    if a == 1:
+        return 1
+    return a * factorial_high(a - 1)
+
+
+def fibonacci(a):
+    if a == 0:
+        return 0
+    if a == 1:
+        return 1
+    if a == 2:
+        return 1
+    return fibonacci(a - 1) + fibonacci(a - 2)
+
+
+# def fibonacci_high(a):
+#     # before = a - 2
+#     # after = a - 1
+#     result = 0
+#     for i in range(a):
+#         if i == 0:
+#             result = 0
+#         elif i == 1 & i == 2:
+#             result = 1
+#         else:
+#             result = (i - 1) + (i - 2)
+#         print(result, end=" ")
+#     return result
+
+
 def personal_info(**kwargs):
     for i in kwargs:
         print(i, kwargs[i])
@@ -24,8 +80,18 @@ def change_count():
     print(count)
 
 
-add(1, 2, 3, 4, 5, 6)
-display_hello("hello")
+# add(1, 2, 3, 4, 5, 6)
+# display_hello("hello")
 # LogUtil.print_log("zack", "content")
-personal_info(name="zack", age=24, height=165)
-change_count()
+# personal_info(name="zack", age=24, height=165)
+# change_count()
+# print(add(2, 3))
+# print(add_height(2, 3, multiplication))
+# print(multiplication(2, 3))
+# print(multiplication_height(2, 3))
+# print(factorial(100))
+# print(factorial_high(2))
+for i in range(10):
+    print(fibonacci(i), end=" ")
+
+# fibonacci_high(10)
